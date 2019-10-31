5c8cf71ed417391bc1b3caf500a65cc7e6191dc2
AKuznecovs <andrejs_kuznecovs@inbox.lv>
2019-10-31 10:47:20 +0200

    Updates

---
+++ precache-manifest.4f6b94dd6bcba73ee1bc6b437ed50edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb460878475d25e6c562828e8003ee20",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "cf9cf00967bd0e1ed0f4",
+    "url": "/robofriends-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "789e46dd55d957f91b06",
+    "url": "/robofriends-redux/static/css/main.cf1008d6.chunk.css"
+  },
+  {
+    "revision": "cf9cf00967bd0e1ed0f4",
+    "url": "/robofriends-redux/static/js/2.fb1646ec.chunk.js"
+  },
+  {
+    "revision": "789e46dd55d957f91b06",
+    "url": "/robofriends-redux/static/js/main.b6309849.chunk.js"
+  },
+  {
+    "revision": "76c9838f340d64cc5a33",
+    "url": "/robofriends-redux/static/js/runtime-main.43ce3bec.js"
+  }
+]);
